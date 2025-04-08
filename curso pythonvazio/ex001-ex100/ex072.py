e472aa517c080efd9eff46a9a0182d1a9f7730dd
renanmatias148 <renanmatias148@gmail.com>
2025-04-08 10:03:01 -0300

    Create ex072.py

---
+++ curso pythonvazio/ex001-ex100/ex072.py
@@ -0,0 +1,7 @@
+cont=("zero","um","dois ","tres","quatro","cinco","seis","sete","oito",
+      "nove","dez",'onze','doze',"treze",'catorze','quinze',"dezesseis",
+      "dezessete","dezoito","dezenove","vinte")
+while True:
+    num=int(input("digite um numero entre 0 e 20"))
+
+
